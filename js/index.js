8057abc85e0c9cc1d093c3fe5592d9dc248d9c95
martinspng <martins4melvis14@gmail.com>
2024-08-11 13:35:07 +0100

    my commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+/*function handleSubmitBtn() {
+    // Get the form inputs
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+  
+    // Validate the form inputs
+    
+  
+  document.getElementById("submitBtn").addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent the form from submitting normally
+    // Make an API call to submit the login data
+    // Here, we're just printing the login object for demonstration purposes
+    handleSubmitBtn();
+  });
+  if (!password || !email) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "password and email are required",
+      });
+      return false;
+    } else {
+      Swal.fire({
+        title: "Good job!",
+        text: "Authentication successful",
+        icon: "success",
+      });
+      document.getElementById("password").value = "";
+      document.getElementById("email").value = "";
+    }
+  }
+
+alert(you 
+Swal.fire(    
+        icon: "error",
+        title: "Oops...",
+        
+        ))*/
+
+alert(hello world)
